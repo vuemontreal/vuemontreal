f177f667d27e02b2c757e0b1972394e2828fccf8
giraud florent <feltron@girauds-MacBook-Pro.local>
2019-08-22 14:59:19 -0400

    feat(init): init vuemontreal blog + domain

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd blog/.vuepress/dist
+
+# if you are deploying to a custom domain
+echo 'www.vuemontreal.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy vuepress montreal blog'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:vuemontreal/vuemontreal.git master:gh-pages
+
+cd -
