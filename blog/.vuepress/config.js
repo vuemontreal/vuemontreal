b7f7cd195f9569bd4f21f5f30291e0f7231ef782
giraud florent <feltron@girauds-MacBook-Pro.local>
2019-08-22 17:38:55 -0400

    feat(init): init vuemontreal blog + domain

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,124 @@
+module.exports = {
+  description: "Vue Montreal Meetups actuality",
+  theme: "thindark",
+  themeConfig: {
+    logo: "/vue-logo-website-trimmed-transparent.png",
+    lastUpdated: "Last Updated",
+    editLinks: true,
+    nav: [
+      { text: "Events", link: "/events/" },
+      { text: "Archives", link: "/archives/" },
+      { text: "Github", link: "https://github.com/vuemontreal/vuemontreal" }
+    ]
+  },
+  head: [
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "57x57",
+        href: "/favicons-vuemontreal/favicon57.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "60x60",
+        href: "/favicons-vuemontreal/favicon60.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "72x72",
+        href: "/favicons-vuemontreal/favicon72.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "76x76",
+        href: "/favicons-vuemontreal/favicon76.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "114x114",
+        href: "/favicons-vuemontreal/favicon114.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "120x120",
+        href: "/favicons-vuemontreal/favicon120.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "144x144",
+        href: "/favicons-vuemontreal/favicon144.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "152x152",
+        href: "/favicons-vuemontreal/favicon152.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicons-vuemontreal/favicon180.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicons-vuemontreal/favicon-32.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        href: "/favicons-vuemontreal/favicon-96.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicons-vuemontreal/favicon-16.png"
+      }
+    ]
+  ],
+  plugins: [
+    ["@vuepress/register-components"],
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-146295749-1"
+      }
+    ]
+  ]
+};
--- blog/readme.md
+++ blog/readme.md
@@ -1,1 +1,16 @@
-# Working progress
+---
+home: true
+heroImage: /vue-logo-website-big-city.png
+heroText: Vue.js Montreal
+tagline: Find our last Meetups!
+actionText: Events →
+actionLink: /events/
+features:
+  - title: Conferences
+    details: Mark your calendar for conferences with dedicated tracks to your favorite framework and keep track of call for proposals!
+  - title: Meetups
+    details: Find local meetup groups and meet with fellow Vue.js enthusiasts!
+  - title: Workshops
+    details: Wondering when the next in-person training session is going to happen? You can find all that information here too!
+footer: MIT Licensed @vue-montreal
+---
