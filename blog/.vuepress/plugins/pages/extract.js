bc75dc0d6aa34bee90d0052c90abc916986ec670
giraud florent <feltron@girauds-MacBook-Pro.local>
2019-08-23 15:56:49 -0400

    feat(chore): adding contributors guide and development

--- blog/.vuepress/plugins/pages/extract.js
+++ blog/.vuepress/plugins/pages/extract.js
@@ -1,19 +1,18 @@
-const fs = require('fs-extra');
+const fs = require("fs-extra");
 
 module.exports = (files = []) => {
-    return files.map(file => {
+  return files.map(file => {
+    const data = fs.readFileSync(file, "utf8");
 
-        const data = fs.readFileSync(file, 'utf8');
+    const splitted = file.split("/");
+    const len = splitted.length;
 
-        const splitted = file.split('/');
-        const len = splitted.length;
+    if (len <= 1) return "";
 
-        if (len <= 1) return '';
-
-        return {
-            data,
-            relativePath: [splitted[len - 2], splitted[len - 1]].join('/'),
-            absolutePath: file
-        };
-    })
-}
+    return {
+      data,
+      relativePath: [splitted[len - 2], splitted[len - 1]].join("/"),
+      absolutePath: file
+    };
+  });
+};
