82ef5b2cf9c4573bb047da2a0a6725c700da7591
giraud florent <feltron@girauds-MacBook-Pro.local>
2019-08-25 00:21:56 -0400

    feat(css): better css organisation and refacto

--- blog/.vuepress/components/meetup.vue
+++ blog/.vuepress/components/meetup.vue
@@ -1,32 +1,31 @@
 <template>
-  <div class="card-wrapper" v-if="endValues">
-    <div class="card-meetup-left">
-      <img :src="endValues.backgroundImg" />
+  <div class="Mtl" v-if="endValues">
+    <div class="Mtl-left">
+      <img :src="endValues.backgroundImg" class="Left-img" />
       <person v-for="person in endValues.datas" :key="person.author" :person="person" />
     </div>
-    <div class="card-meetup-right">
-      <dir class="card-meetup_body">
-        <div class="white block big">
+    <div class="Mtl-right Right">
+      <div class="Right-content Content white--dark bg-dark">
+        <div class="Place-info Info">
           <vp-icon name="clock" />
-          <span class="pl-4">
+          <span class="Title">
             {{endValues.date}}
             {{endValues.hour}}
           </span>
         </div>
-        <div class="white block">
+        <div class="Place-info Info">
           <vp-icon name="map" />
-          <span class="pl-4">{{endValues.location}}</span>
+          <span class="Title">{{endValues.location}}</span>
         </div>
-        <iframe
-          :src="endValues.gmapIframe"
-          height="250"
-          frameborder="0"
-          style="border:0; width: 100%; margin-top: 12px; margin-bottom: 12px;"
-        ></iframe>
-        <div class="btn-block">
-          <a class="btn-3d green" :href="endValues.eventLink" target="_blank">Inscription</a>
+        <iframe :src="endValues.gmapIframe" height="250" frameborder="0" class="Place-map"></iframe>
+        <div class="Place-action">
+          <a
+            class="Btn Btn--3d white bg-green"
+            :href="endValues.eventLink"
+            target="_blank"
+          >Inscription</a>
         </div>
-      </dir>
+      </div>
     </div>
   </div>
 </template>
@@ -47,3 +46,126 @@ export default {
   }
 };
 </script>
+
+<style>
+.theme-default-content:not(.custom) {
+  margin: 0;
+  min-width: 100%;
+}
+
+.Mtl {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.Mtl-left {
+  flex-grow: 1;
+}
+
+.Left-img {
+  width: 100%;
+}
+
+.Mtl-right {
+  max-width: 350px;
+  padding-bottom: 20px;
+}
+
+.Right {
+  padding-left: 8px;
+  padding-right: 8px;
+}
+
+.Right-content {
+  top: 60px;
+  position: sticky;
+  margin-top: 0;
+}
+
+.Content {
+  box-shadow: 1px 6px 8px -2px rgba(0, 0, 0, 0.51);
+  border-radius: 4px;
+  padding-top: 16px;
+  padding-left: 0;
+}
+
+.Place-info {
+  margin-top: 12px;
+  margin-bottom: 18px;
+}
+
+.Info {
+  padding-left: 30px;
+  padding-right: 12px;
+}
+
+.Title {
+  padding-left: 18px;
+  font-weight: 600;
+}
+
+.Place-map {
+  border: 0;
+  width: 100%;
+  margin-top: 12px;
+  margin-bottom: 12px;
+}
+
+.Place-action {
+  text-align: center;
+  padding-bottom: 12px;
+}
+
+.Btn {
+  text-align: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  display: inline-block;
+  cursor: pointer;
+  font-size: 18px;
+}
+
+.Btn--3d {
+  position: relative;
+  box-shadow: 0 0 0 1px #103320 inset, 0 0 0 2px rgba(255, 255, 255, 0.15) inset;
+  /* padding: 5px 20px; */
+  border-radius: 6px;
+  transition: top 0.01s linear;
+  text-shadow: 0 1px 0 rgba(0, 0, 0, 0.15);
+}
+
+.Links {
+  display: flex;
+  justify-content: flex-end !important;
+}
+
+@media (max-width: 1160px) {
+  .theme-default-content:not(.custom) {
+    margin-left: 0 !important;
+    margin-right: 0 !important;
+    padding-left: 0 !important;
+    padding-right: 0 !important;
+    width: 80% !important;
+    margin: auto !important;
+    min-width: inherit;
+  }
+
+  .Mtl {
+    flex-flow: column-reverse;
+  }
+
+  .Mtl-right {
+    margin-left: 0 !important;
+    margin-right: 0 !important;
+    min-width: 100%;
+    margin: auto;
+  }
+
+  .Right {
+    padding-right: 0 !important;
+    padding-left: 0 !important;
+  }
+}
+</style>
--- blog/.vuepress/components/person.vue
+++ blog/.vuepress/components/person.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
     <h2 :id="person.title" v-html="person.title"></h2>
-    <div class="person">
-      <div class="person-img-block">
+    <div class="Person">
+      <div class="Person-img Img">
         <a :href="person.profile" target="_blank">
-          <img :src="person.img" width="100px;" alt="Giraud Florent" />
+          <img :src="person.img" width="100" alt="Giraud Florent" />
           <br />
           <sub>
             <b>{{person.author}}</b>
           </sub>
         </a>
       </div>
-      <div class="person-description-block">
-        <div class="person-infos-block">
-          <a v-if="person.video" :href="person.video" target="_blank" class="mr-6">
+      <div class="Person-description">
+        <div class="Description-infos">
+          <a v-if="person.video" :href="person.video" target="_blank">
             <vp-icon name="eye" />
-            <span class="person-text">Movie</span>
+            <span class="Infos-text">Movie</span>
           </a>
           <a v-if="person.slide" :href="person.slide" target="_blank">
             <vp-icon name="qianbao" />
-            <span class="person-text">Slide</span>
+            <span class="Infos-text">Slide</span>
           </a>
         </div>
         <p v-html="person.description"></p>
-        <div class="tag-container">
-          <h4 v-for="(tag, i) in person.tags" :key="i" class="tag mr-6">
-            <span class="person-text">{{tag}}</span>
+        <div class="Infos-tags Tags">
+          <h4 v-for="(tag, i) in person.tags" :key="i" class="Tag">
+            <span class="Infos-text">{{tag}}</span>
           </h4>
         </div>
       </div>
@@ -45,15 +45,35 @@ export default {
 </script>
 
 <style>
-.mr-6 {
-  margin-right: 6px;
+.Person {
+  display: flex;
+  max-width: 800px;
+}
+.Person-img {
+  width: 150px;
 }
 
-.tag-container {
+.Person-description {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.Description-infos h4 {
+  margin: 0;
+  margin-right: 8px;
+  display: inline-block;
+}
+
+.Infos-text {
+  margin-left: 4px;
+}
+
+.Infos-tags {
   display: flex;
 }
 
-.tag {
+.Tag {
   background: rgba(0, 0, 0, 0.05);
   background-image: initial;
   background-position-x: initial;
@@ -64,5 +84,6 @@ export default {
   background-clip: initial;
   background-color: rgba(0, 0, 0, 0.05);
   padding: 8px;
+  margin-right: 6px;
 }
 </style>
