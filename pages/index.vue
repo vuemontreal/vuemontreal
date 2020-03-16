eef539c8cdb187786d12e0402f11ffacd1ba435a
Loïc <fuentes.loic@gmail.com>
2020-03-16 01:07:17 -0400

    basic event list without any style (#76)
    Co-authored-by: Loïc Fuentes <loic.fuentes@aerial.ai>

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,45 @@
 <template>
-  <div>index</div>
+  <section class="events">
+    <article v-for="event in events" class="event">
+      <header>
+        <time>{{ event.sort_by_date }}</time>
+        <h1>{{ event.content.title }}</h1>
+
+        <div
+          v-if="event.content.sponsors && event.content.sponsors.length"
+          class="sponsor"
+        >
+          <p>Sponsors:</p>
+          <ul>
+            <li v-for="sponsor in event.content.sponsors">
+              <a :href="sponsor.link.url" target="_blank">
+                <img :src="sponsor.image" :alt="sponsor.name" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+      <main>
+        <p>{{ event.content.preview }}</p>
+      </main>
+      <footer>
+        <a
+          v-if="event.content.inscription.url"
+          :href="event.content.inscription.url"
+          target="_blank"
+          >Register</a
+        >
+        <nuxt-link :to="event.full_slug" class="button">
+          <span class="pb-1">More</span>
+        </nuxt-link>
+      </footer>
+    </article>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
-  computed: {},
   asyncData({ app, error }) {
     return app.$storyapi
       .get('cdn/stories', {
@@ -14,8 +48,7 @@ export default {
         sort_by: 'sort_by_date:asc'
       })
       .then((res) => {
-        // console.log(res)
-        return res.data
+        return { events: res.data.stories }
       })
       .catch((res) => {
         if (!res.response) {
@@ -37,3 +70,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+}
+
+.event {
+  border: 1px solid black;
+  margin-bottom: 2rem;
+  padding: 2rem;
+}
+</style>
