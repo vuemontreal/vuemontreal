a71027ca21b2cb02508f8b96eee24d8d0e2cb535
Aurélien Roux <aureroux13@gmail.com>
2020-03-22 12:36:00 -0400

    responsive carousel swiper

---
+++ components/carousel.vue
@@ -0,0 +1,63 @@
+<template>
+  <swiper :options="swiperOption" class="swiper">
+    <swiper-slide>
+      <img
+        src="https://images.unsplash.com/photo-1470181942237-78ce33fec141?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        alt=""
+      />
+    </swiper-slide>
+    <swiper-slide>
+      <img
+        src="https://images.unsplash.com/photo-1526669754135-c1babeb8c542?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80"
+        alt=""
+      />
+    </swiper-slide>
+    <swiper-slide>
+      <img
+        src="https://images.unsplash.com/photo-1470181942237-78ce33fec141?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        alt=""
+      />
+    </swiper-slide>
+    <swiper-slide>
+      <img
+        src="https://images.unsplash.com/photo-1526669754135-c1babeb8c542?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80"
+        alt=""
+      />
+    </swiper-slide>
+
+    <div slot="button-prev" class="swiper-button-prev"></div>
+    <div slot="button-next" class="swiper-button-next"></div>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
+export default {
+  name: 'SwiperExampleNavigation',
+  title: 'Navigation',
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  data() {
+    return {
+      swiperOption: {
+        autoHeight: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.swiper {
+  height: 300px;
+  max-width: 100%;
+}
+</style>
