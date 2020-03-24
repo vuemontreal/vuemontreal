c451306759472bcb58e347b362a0412b45352509
f3ltron <florent.giraud.16@gmail.com>
2020-03-24 01:08:27 -0400

    add analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,13 @@ export default {
   },
   buildModules: [
     '@nuxtjs/eslint-module',
-    '@nuxtjs/tailwindcss'
-    // '@nuxtjs/dotenv'
+    '@nuxtjs/tailwindcss',
+    [
+      '@nuxtjs/google-analytics',
+      {
+        id: process.env.GOOGLE_ANALYTIC
+      }
+    ]
   ],
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
