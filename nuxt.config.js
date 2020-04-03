9716734e11c7289f9c14afdc613050dc71259f0a
f3ltron <florent.giraud.16@gmail.com>
2020-04-03 00:34:14 -0400

    add env now

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,34 @@
 /* eslint-disable */
 
 require('dotenv').config()
+const axios = require('axios')
+
+const generateStoryblokEventRoutes = function() {
+  return new Promise((resolve, reject) => {
+    const storyblokUrl = 'https://api.storyblok.com/v1/cdn/stories'
+    const token = process.env.STORYBLOK_TOKEN
+    const version = process.env.STORYBLOK_VERSION || 'draft'
+    const endUrl = `?token=${token}&version=${version}&cv=${new Date().getTime()}`
+
+    const langs = ['fr', 'en']
+
+    const storyblokEventsRoutes = []
+
+    langs.forEach((lang) => {
+      storyblokEventsRoutes.push(
+        axios.get(`${storyblokUrl}${endUrl}&starts_with=${lang}/events/`)
+      )
+    })
+
+      const ret = []
+    return Promise.all(storyblokEventsRoutes).then((response) => {
+        response.map((res) => {
+          res.data.stories.map((story) => ret.push(story.full_slug))
+        })
+        resolve(ret);
+      }).catch(reject)
+  })
+}
 
 
 export default {
@@ -21,7 +49,12 @@ export default {
   server: require('./configs/server'),
   buildModules: require('./configs/buildModules'),
   modules: require('./configs/modules'),
-  generate: require('./configs/generate').generate,
-  sitemap: require('./configs/generate').sitemap,
+  generate: {
+    routes: generateStoryblokEventRoutes
+  },
+  sitemap:{
+    hostname: 'https://vuemontreal.org',
+    routes: generateStoryblokEventRoutes
+  },
   i18n: require('./configs/i18n'),
 }
