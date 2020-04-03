a8640de4da7d6aa81bd8cfc722d93b37a9e4161e
f3ltron <florent.giraud.16@gmail.com>
2020-04-03 01:24:09 -0400

    trying deploy on surge with workflow

--- configs/generate.js
+++ configs/generate.js
@@ -3,31 +3,33 @@
 require('dotenv').config()
 const axios = require('axios')
 
-const generateStoryblokEventRoutes = function() {
-  return new Promise((resolve, reject) => {
-    const storyblokUrl = 'https://api.storyblok.com/v1/cdn/stories'
-    const token = process.env.STORYBLOK_TOKEN
-    const version = process.env.STORYBLOK_VERSION || 'draft'
-    const endUrl = `?token=${token}&version=${version}&cv=${new Date().getTime()}`
+const generateStoryblokEventRoutes = async () => {
+  const storyblokUrl = 'https://api.storyblok.com/v1/cdn/stories'
+  const token = process.env.STORYBLOK_TOKEN
+  const version = process.env.STORYBLOK_VERSION || 'draft'
+  const endUrl = `?token=${token}&version=${version}&cv=${new Date().getTime()}`
 
-    const langs = ['fr', 'en']
+  const langs = ['fr', 'en']
 
-    const storyblokEventsRoutes = []
+  const storyblokEventsRoutes = []
 
-    langs.forEach((lang) => {
-      storyblokEventsRoutes.push(
-        axios.get(`${storyblokUrl}${endUrl}&starts_with=${lang}/events/`)
-      )
+  langs.forEach((lang) => {
+    storyblokEventsRoutes.push(
+      axios.get(`${storyblokUrl}${endUrl}&starts_with=${lang}/events/`)
+    )
+  })
+
+  try {
+    const ret = []
+    const response = await Promise.all(storyblokEventsRoutes)
+    response.map((res) => {
+      res.data.stories.map((story) => ret.push(story.full_slug))
     })
 
-      const ret = []
-    return Promise.all(storyblokEventsRoutes).then((response) => {
-        response.map((res) => {
-          res.data.stories.map((story) => ret.push(story.full_slug))
-        })
-        resolve(ret);
-      }).catch(reject)
-  })
+    return ret
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,6 @@
 /* eslint-disable */
 
 require('dotenv').config()
-const axios = require('axios')
-
-const { STORYBLOK_TOKEN, STORYBLOK_VERSION, GOOGLE_ANALYTIC } = process.env;
-console.log(STORYBLOK_TOKEN, STORYBLOK_VERSION, GOOGLE_ANALYTIC);
-
-const generateStoryblokEventRoutes = function() {
-  return new Promise((resolve, reject) => {
-    const storyblokUrl = 'https://api.storyblok.com/v1/cdn/stories'
-    const token = process.env.STORYBLOK_TOKEN
-    const version = process.env.STORYBLOK_VERSION || 'draft'
-    const endUrl = `?token=${token}&version=${version}&cv=${new Date().getTime()}`
-
-    const langs = ['fr', 'en']
-
-    const storyblokEventsRoutes = []
-
-    langs.forEach((lang) => {
-      storyblokEventsRoutes.push(
-        axios.get(`${storyblokUrl}${endUrl}&starts_with=${lang}/events/`)
-      )
-    })
-
-      const ret = []
-    return Promise.all(storyblokEventsRoutes).then((response) => {
-        response.map((res) => {
-          res.data.stories.map((story) => ret.push(story.full_slug))
-        })
-        resolve(ret);
-      }).catch(err => {
-        console.log("ERROR", err);
-        reject(err)
-      })
-  })
-}
 
 
 export default {
@@ -55,12 +21,7 @@ export default {
   server: require('./configs/server'),
   buildModules: require('./configs/buildModules'),
   modules: require('./configs/modules'),
-  generate: {
-    routes: generateStoryblokEventRoutes
-  },
-  sitemap:{
-    hostname: 'https://vuemontreal.org',
-    routes: generateStoryblokEventRoutes
-  },
+  generate: require('./configs/generate').generate,
+  sitemap: require('./configs/generate').sitemap,
   i18n: require('./configs/i18n'),
 }
