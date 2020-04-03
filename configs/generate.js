eade9705811ad75ee4d953342947c4eb55b9d7c3
f3ltron <florent.giraud.16@gmail.com>
2020-04-03 00:03:08 -0400

    add env now

--- configs/generate.js
+++ configs/generate.js
@@ -3,33 +3,31 @@
 require('dotenv').config()
 const axios = require('axios')
 
-const generateStoryblokEventRoutes = async () => {
-  const storyblokUrl = 'https://api.storyblok.com/v1/cdn/stories'
-  const token = process.env.STORYBLOK_TOKEN
-  const version = process.env.STORYBLOK_VERSION || 'draft'
-  const endUrl = `?token=${token}&version=${version}&cv=${new Date().getTime()}`
+const generateStoryblokEventRoutes = function() {
+  return new Promise((resolve, reject) => {
+    const storyblokUrl = 'https://api.storyblok.com/v1/cdn/stories'
+    const token = process.env.STORYBLOK_TOKEN
+    const version = process.env.STORYBLOK_VERSION || 'draft'
+    const endUrl = `?token=${token}&version=${version}&cv=${new Date().getTime()}`
 
-  const langs = ['fr', 'en']
+    const langs = ['fr', 'en']
 
-  const storyblokEventsRoutes = []
+    const storyblokEventsRoutes = []
 
-  langs.forEach((lang) => {
-    storyblokEventsRoutes.push(
-      axios.get(`${storyblokUrl}${endUrl}&starts_with=${lang}/events/`)
-    )
-  })
-
-  try {
-    const ret = []
-    const response = await Promise.all(storyblokEventsRoutes)
-    response.map((res) => {
-      res.data.stories.map((story) => ret.push(story.full_slug))
+    langs.forEach((lang) => {
+      storyblokEventsRoutes.push(
+        axios.get(`${storyblokUrl}${endUrl}&starts_with=${lang}/events/`)
+      )
     })
 
-    return ret
-  } catch (e) {
-    console.log(e)
-  }
+      const ret = []
+    return Promise.all(storyblokEventsRoutes).then((response) => {
+        response.map((res) => {
+          res.data.stories.map((story) => ret.push(story.full_slug))
+        })
+        resolve(ret);
+      }).catch(reject)
+  })
 }
 
 
