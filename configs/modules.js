5555b4f06201f48db762b08cdbf1c5b855a47faa
unknown <jerembardon@gmail.com>
2020-04-04 13:30:51 -0400

    COrrect all merge

--- configs/modules.js
+++ configs/modules.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-/* eslint-disable */
-
-require('dotenv').config()
-
-module.exports = [
-  // Doc: https://axios.nuxtjs.org/usage
-  '@nuxtjs/axios',
-  '@nuxtjs/pwa',
-  // Doc: https://github.com/nuxt-community/dotenv-module
-  [
-    'storyblok-nuxt',
-    {
-      accessToken: process.env.STORYBLOK_TOKEN,
-      cacheProvider: 'memory'
-    }
-  ],
-  [
-    'nuxt-fontawesome',
-    {
-      imports: [
-        {
-          set: '@fortawesome/free-solid-svg-icons',
-          icons: ['fas']
-        },
-        {
-          set: '@fortawesome/free-brands-svg-icons',
-          icons: ['fab']
-        }
-      ]
-    }
-  ],
-  'nuxt-i18n',
-  '@nuxtjs/sitemap'
-]
-=======
 /* eslint-disable */
 
 require('dotenv').config()
@@ -68,4 +32,3 @@ module.exports = [
   'nuxt-i18n',
   '@nuxtjs/sitemap',
 ]
->>>>>>> vuemontreal-next
