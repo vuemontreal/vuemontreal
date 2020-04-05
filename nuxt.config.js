caaeb6d151422055f7936968474db2b8d3578cdf
fgiraud <fgiraud@protonmail.com>
2020-04-04 20:23:52 -0400

    delete deploy sh + test auto deployement on pr + update + clean nuxt … (#126)
    * delete bot workflow
    
    * update version on home page to not draft on netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,23 +8,7 @@ export default {
   env: {
     version: process.env.STORYBLOK_VERSION || 'draft',
   },
-  fallback: false,
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      // { rel: 'stylesheet', href: 'https://unpkg.com/swiper/css/swiper.min.css' }
-    ],
-  },
+  head: require('./configs/head'),
   server: require('./configs/server'),
   buildModules: require('./configs/buildModules'),
   modules: require('./configs/modules'),
