cc79ac98e60864a119ca48dc5bf80c323e2d6fd5
giraud florent <giraudflorent@girauds-MacBook-Pro.local>
2020-05-02 14:46:52 -0400

    storybook: component now mount but tailwind not apply

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,4 @@
-const path = require('path');
 const { getWebpackConfig } = require('nuxt')
-
-// function isObject(item) {
-//     return (item && typeof item === 'object' && !Array.isArray(item));
-//   }
-  
-// function mergeDeep(target, ...sources) {
-//     if (!sources.length) return target;
-//     const source = sources.shift();
-  
-//     if (isObject(target) && isObject(source)) {
-//       for (const key in source) {
-//         if (isObject(source[key])) {
-//           if (!target[key]) Object.assign(target, { [key]: {} });
-//           mergeDeep(target[key], source[key]);
-//         } else {
-//           Object.assign(target, { [key]: source[key] });
-//         }
-//       }
-//     }
-  
-//     return mergeDeep(target, ...sources);
-//   }
 
 module.exports = {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
@@ -42,7 +19,7 @@ module.exports = {
             module: {
                 rules: [
                     ...nuxtWebpack.module.rules,
-                    ...sbWebpack.module.rules
+                    // ...sbWebpack.module.rules
                 ]
             },
             plugins: [
--- components/test.stories.js
+++ components/test.stories.js
@@ -3,8 +3,6 @@ import Vue from 'vue'
 import { storiesOf } from '@storybook/vue'
 import Test from '@/components/test.vue'
 
-console.log(Test)
-
 storiesOf('tests', module).add('Test', () => ({
   components: { Test },
   template: '<Test />'
