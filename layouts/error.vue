3e2230fcb50f7894b61e4f125df804c8b74d5a80
giraud florent <hello@florent.dev>
2020-06-19 17:43:12 -0400

    adding 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-wrap items-start justify-center">
+    <div class="mb-8">
+      <img src="~assets/notfound.png" alt="" />
+    </div>
+    <div class="flex justify-center w-full mb-12">
+      <mtl-button @click.native="$router.back()">GO BACK</mtl-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // this.$ga.event({
+    //   eventCategory: 'error',
+    //   eventAction: 'navigation',
+    //   eventLabel: window.location.href
+    // })
+  }
+}
+</script>
