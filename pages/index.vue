09913c3cbe9cc4db759e5e2dcee8f1ef9e7f446d
giraud florent <hello@florent.dev>
2020-06-19 17:14:03 -0400

    adding job link connect to storyblok and add example cards

--- pages/index.vue
+++ pages/index.vue
@@ -81,11 +81,11 @@
     <section id="new-jobs" v-if="datas" class="px-2 lg:px-0 mb-8">
       <div class="flex justify-between items-center mb-8">
         <mtl-h-2>{{ datas.job_title }}</mtl-h-2>
-        <nuxt-link :to="localePath(datas.job_next_link)">
+        <a :href="datas.job_next_link" target="_blank">
           <mtl-text-info class=" text-mtl-green-500">
             {{ datas.job_next_text }}
           </mtl-text-info>
-        </nuxt-link>
+        </a>
       </div>
       <div class="flex flex-wrap">
         <div class="w-full px-4 lg:w-1/2 mb-4">
@@ -125,20 +125,94 @@
           </mtl-card-job>
         </div>
         <div class="w-full px-4 lg:w-1/2 mb-4">
-          <mtl-card-job
-            class="bg-mtl-black-200 flex justify-center items-center h-64 lg:h-full"
-          >
+          <mtl-card-job>
+            <template #card-header>
+              <mtl-h-4 class="mb-2 text-mtl-green-500 pt-4">New</mtl-h-4>
+              <mtl-text-info
+                class="bg-mtl-black-200 absolute top-0 right-0 text-mtl-black-400 rounded-bl-lg px-4 py-2"
+              >
+                Full-time
+              </mtl-text-info>
+            </template>
+            <template #card-hero>
+              <div
+                class="w-10 h-10 bg-mtl-black-400 rounded-lg mr-6 mt-1"
+              ></div>
+              <div class="flex-grow">
+                <mtl-h-3 class="text-mtl-black-400">
+                  Senior Front-End Developer
+                </mtl-h-3>
+                <mtl-text-info class="text-mtl-black-200">
+                  At <span class="text-mtl-black-500">Sigma</span> San
+                  Francisco, CA, USA
+                </mtl-text-info>
+              </div>
+            </template>
             <template #card-body>
-              <mtl-paragraph>Post a job</mtl-paragraph>
+              <mtl-paragraph class="text-mtl-black-400">
+                The "orga" type meetings deal with topics about the organization
+                of the Vue.js Montreal community. The goal of this meeting is to
+                present the new vuemontreal.org website ...
+              </mtl-paragraph>
+            </template>
+            <template #card-actions>
+              <mtl-button>apply</mtl-button>
             </template>
           </mtl-card-job>
+        </div>
+        <div class="w-full px-4 lg:w-1/2 mb-4">
+          <mtl-card-job>
+            <template #card-header>
+              <mtl-h-4 class="mb-2 text-mtl-green-500 pt-4">New</mtl-h-4>
+              <mtl-text-info
+                class="bg-mtl-black-200 absolute top-0 right-0 text-mtl-black-400 rounded-bl-lg px-4 py-2"
+              >
+                Full-time
+              </mtl-text-info>
+            </template>
+            <template #card-hero>
+              <div
+                class="w-10 h-10 bg-mtl-black-400 rounded-lg mr-6 mt-1"
+              ></div>
+              <div class="flex-grow">
+                <mtl-h-3 class="text-mtl-black-400">
+                  Senior Front-End Developer
+                </mtl-h-3>
+                <mtl-text-info class="text-mtl-black-200">
+                  At <span class="text-mtl-black-500">Sigma</span> San
+                  Francisco, CA, USA
+                </mtl-text-info>
+              </div>
+            </template>
+            <template #card-body>
+              <mtl-paragraph class="text-mtl-black-400">
+                The "orga" type meetings deal with topics about the organization
+                of the Vue.js Montreal community. The goal of this meeting is to
+                present the new vuemontreal.org website ...
+              </mtl-paragraph>
+            </template>
+            <template #card-actions>
+              <mtl-button>apply</mtl-button>
+            </template>
+          </mtl-card-job>
+        </div>
+        <div class="w-full px-4 lg:w-1/2 mb-4">
+          <a :href="datas.job_post_link" target="_blank">
+            <mtl-card-job
+              class="bg-mtl-black-200 flex justify-center items-center h-64 lg:h-full"
+            >
+              <template #card-body>
+                <mtl-paragraph>{{ datas.job_post_text }}</mtl-paragraph>
+              </template>
+            </mtl-card-job>
+          </a>
         </div>
       </div>
     </section>
     <section id="sponsors" v-if="datas" class="bg-mtl-black-200 py-6">
-      <mtl-h-2 class="text-center mb-8">{{
-        datas.sponsor_title || ''
-      }}</mtl-h-2>
+      <mtl-h-2 class="text-center mb-8">
+        {{ datas.sponsor_title || '' }}
+      </mtl-h-2>
       <template v-if="datas.sponsors.length">
         <ul class="flex flex-wrap m-auto w-full mb-8">
           <li
