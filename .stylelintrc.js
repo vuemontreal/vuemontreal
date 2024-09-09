21bf35769f090ec523d51ff8c91143c7ac9ee89c
Tommy Lebel <tommy@tomicio.com>
2024-09-09 16:08:00 -0600

    add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    'stylelint-config-recommended-vue',
+    'stylelint-config-tailwindcss',
+  ],
+  plugins: [
+    'stylelint-order',
+  ],
+  rules: {
+    'order/properties-alphabetical-order': true,
+    'at-rule-no-unknown': null,
+    'order/order': [
+      'custom-properties',
+      'declarations'
+    ]
+  }
+}
