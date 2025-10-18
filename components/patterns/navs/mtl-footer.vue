19714ba226a47c91be02df4d3538c103da8f1426
Massimo Russo <russo.mass@gmail.com>
2025-10-18 10:53:24 -0400

    Update LinkedIn link in footer component

--- components/patterns/navs/mtl-footer.vue
+++ components/patterns/navs/mtl-footer.vue
@@ -13,7 +13,7 @@
       </a>
       <a
         class="p-2 icon"
-        href="https://www.linkedin.com/groups/12289604/"
+        href="https://www.linkedin.com/company/vue-montreal/"
         target="_blank"
         rel="noopener noreferrer"
       >
